089f612bfb24b2f37a9448ebe64e867cf5269a9a
Jared Tasker <jared@needapart.co.nz>
2019-06-21 11:19:09 +1200

    make Board a class component

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import Card from './Card'
 
-const Board = () => {
-  return (
-    <>
-    <div className="leftcontainer">
-      <Card />
-    </div>
-    <div>
-      <p>or:</p>
-    </div>
-    <div className="rightcontainer">
-      <Card />
-    </div>
-    </>
-  )
+class Board extends React.Component {
+  render () {
+    return (
+      <>
+      <div className="leftcontainer">
+        <Card />
+      </div>
+      <div>
+        <p>or:</p>
+      </div>
+      <div className="rightcontainer">
+        <Card />
+      </div>
+      </>
+    )
+  }
 }
 
 export default Board
