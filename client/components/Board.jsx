8ef78771795b4b8fdfb763952c07d8e1ec7ad88e
Jared Tasker <jared@needapart.co.nz>
2019-06-21 12:05:42 +1200

    wps: getCards()

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -5,11 +5,20 @@ import data from '../../data/wur'
 
 class Board extends React.Component {
   getCards () {
-
+    
+    cards = []
+    for (let i = 0; i < 2; i++) {
+      cardId = () => {data[Math.floor(Math.random()*data.length)]}
+      if(!cards.includes(cardId)){
+        cards.push(cardId)
+      }
+    }
   }
 
+
   render () {
-    console.log(data)
+    card = 
+
     return (
       <>
       <div className="leftcontainer">
